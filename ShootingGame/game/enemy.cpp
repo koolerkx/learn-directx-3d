1768f823129d5cd8edea5872e05e1b14458e4f9f
Kooler Fan <koolerkx@gmail.com>
2025-07-02 10:00:54 +0900

    game: implement enemy

--- ShootingGame/game/enemy.cpp
+++ ShootingGame/game/enemy.cpp
@@ -7,8 +7,35 @@
 
 #include "enemy.h"
 
+#include <DirectXMath.h>
+
+#include "color.h"
+#include "sprite.h"
+#include "texture.h"
+using namespace DirectX;
+
+struct Enemy
+{
+    XMFLOAT2 position;
+    XMFLOAT2 velocity;
+    bool isEnable;
+};
+
+static constexpr unsigned int ENEMY_MAX = 256;
+static Enemy g_Enemys[ENEMY_MAX]{};
+static int g_Enemy_Texid = -1;
+
+static constexpr XMFLOAT2 ENEMY_SIZE = {32.0f, 48.0f};
+static constexpr XMFLOAT2 ENEMY_SPEED = {-100.0f, 0.0f};
+
 void Enemy_Initialize()
 {
+    for (Enemy& enemy : g_Enemys)
+    {
+        enemy.isEnable = false;
+    }
+
+    g_Enemy_Texid = Texture_Load(L"assets/white.png");
 }
 
 void Enemy_Finalize()
@@ -17,12 +44,50 @@ void Enemy_Finalize()
 
 void Enemy_Update(double elapsed_time)
 {
+    for (Enemy& enemy : g_Enemys)
+    {
+        if (!enemy.isEnable) continue;
+
+        // 位置管理
+        XMVECTOR position = XMLoadFloat2(&enemy.position);
+        XMVECTOR velocity = XMLoadFloat2(&enemy.velocity);
+
+        position += velocity * elapsed_time;
+
+        XMStoreFloat2(&enemy.position, position);
+        XMStoreFloat2(&enemy.velocity, velocity);
+
+
+        if (enemy.position.x + ENEMY_SIZE.x < 0.0f)
+        {
+            enemy.isEnable = false;
+        }
+    }
 }
 
 void Enemy_Draw()
 {
+    for (Enemy& enemy : g_Enemys)
+    {
+        if (!enemy.isEnable) continue;
+
+        Sprite_Draw(g_Enemy_Texid,
+                    enemy.position.x, enemy.position.y,
+                    ENEMY_SIZE.x, ENEMY_SIZE.y, 0, Color::RED
+        );
+    }
 }
 
-void Enemy_Create(const DirectX::XMFLOAT2& position)
+void Enemy_Create(const XMFLOAT2& position)
 {
+    for (Enemy& enemy : g_Enemys)
+    {
+        if (enemy.isEnable) continue;
+
+        enemy.isEnable = true;
+        enemy.position = position;
+        enemy.velocity = ENEMY_SPEED;
+
+        break;
+    }
 }
--- ShootingGame/game/game.cpp
+++ ShootingGame/game/game.cpp
@@ -1,12 +1,18 @@
 #include "game.h"
 
 #include "bullet.h"
+#include "direct3d.h"
+#include "enemy.h"
 #include "player.h"
+
+static float fromLastEnemySpawnTime = 0;
+static constexpr float ENEMY_SPAWN_INTERVAL = 1.0f;
 
 void Game_Initialize()
 {
     Player_Initialize({64.0f, 64.0f});
     Bullet_Initialize();
+    Enemy_Initialize();
 }
 
 void Game_Finalize()
@@ -15,12 +21,26 @@ void Game_Finalize()
 
 void Game_Update(double elapsed_time)
 {
+
+    fromLastEnemySpawnTime += elapsed_time;
+    if (fromLastEnemySpawnTime > ENEMY_SPAWN_INTERVAL)
+    {
+        fromLastEnemySpawnTime = 0;
+        
+        Enemy_Create({
+            static_cast<float>(Direct3D_GetBackBufferWidth()) - 100.0f,
+            static_cast<float>(Direct3D_GetBackBufferHeight()) / 2.0f
+        });
+    }
+    
     Player_Update(elapsed_time);
     Bullet_Update(elapsed_time);
+    Enemy_Update(elapsed_time);
 }
 
 void Game_Draw()
 {
+    Enemy_Draw();
     Bullet_Draw();
     Player_Draw();
 }
