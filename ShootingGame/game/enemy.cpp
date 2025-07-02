43f977833e6943e745e94854fc9a15428a7d40ab
Kooler Fan <koolerkx@gmail.com>
2025-07-02 09:41:43 +0900

    game: add enemy files

---
+++ ShootingGame/game/enemy.cpp
@@ -0,0 +1,28 @@
+/**
+ * @file enemy.cpp
+ * @brief 敵機の管理
+ * @author KOOLER FAN
+ * @date 2025/07/02
+ */
+
+#include "enemy.h"
+
+void Enemy_Initialize()
+{
+}
+
+void Enemy_Finalize()
+{
+}
+
+void Enemy_Update(double elapsed_time)
+{
+}
+
+void Enemy_Draw()
+{
+}
+
+void Enemy_Create(const DirectX::XMFLOAT2& position)
+{
+}
---
+++ ShootingGame/game/enemy.h
@@ -0,0 +1,21 @@
+/**
+ * @file enemy.h
+ * @brief 敵機の管理
+ * @author KOOLER FAN
+ * @date 2025/07/02
+ */
+
+#ifndef ENEMY_H
+#define ENEMY_H
+
+#include "DirectXTex.h"
+
+void Enemy_Initialize(void);
+void Enemy_Finalize(void);
+
+void Enemy_Update(double elapsed_time);
+void Enemy_Draw(void);
+
+void Enemy_Create(const DirectX::XMFLOAT2& position);
+
+#endif
